ccaa7994cf526c347c9b6cc2a32f745a39c53cb9
sirStephen <itobitoast@gmail.com>
2018-10-16 01:56:34 +0100

    Added routes(ADD and POST)

--- server/server.js
+++ server/server.js
@@ -1,17 +1,41 @@
 // server.js
 import express from 'express';
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
 
-app.use(express.json())
+const app = express();
 
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
-})
+app.use(express.json());
 
-app.listen(3000)
-console.log('app running on port ', 3000)
+app.get('/', (req, res) => res.status(200).send({
+  message: 'YAY! Congratulations! Your first endpoint is working',
+}));
 
+app.get('/api/v1/products', (request, response) => response.status(200).json({
+  message: 'fetch all products',
+}));
 
+app.get('/api/v1/products/:productid', (request, response) => response.status(200).json({
+  message: 'fetch a single product record',
+}));
 
+app.get('/api/v1/sales', (request, response) => response.status(200).json({
+  message: 'fetch all sales records',
+}));
 
+app.get('/api/v1/sales/:saleid', (request, response) => response.status(200).json({
+  message: 'fetch a single sale record',
+}));
+
+app.post('/api/v1/products', (request, response) => response.status(200).json({
+  message: 'create a product',
+}));
+
+app.post('/api/v1/sales', (request, response) => response.status(200).json({
+  message: 'create a sale order',
+}));
+
+app.listen(process.env.PORT, () => {
+  console.log(`app is listening on ${process.env.PORT}!`);
+});
