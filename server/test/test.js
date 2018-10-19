542dda9550c20961bc8d57b73e99aaa6285211b8
Otiono Gregory <itobitoast@gmail.com>
2018-10-19 15:54:09 +0100

    GET /api/v1/products AND /api/v1/products/:id

---
+++ server/test/test.js
@@ -0,0 +1,27 @@
+import chai, { assert, expect } from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server';
+
+chai.use(chaiHttp);
+
+describe('Test for all products api', () => {
+  it('Get all products', (done) => { // <= Pass in done callback
+    chai.request(server)
+      .get('/api/v1/products')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        done(); // <= Call done to signal callback end
+      });
+  });
+});
+
+describe('Test for a product api', () => {
+  it('Get A products', (done) => { // <= Pass in done callback
+    chai.request(server)
+      .get('/api/v1/products/:id')
+      .end((error, response) => {
+        expect(response).to.have.status(404);
+        done(); // <= Call done to signal callback end
+      });
+  });
+});
