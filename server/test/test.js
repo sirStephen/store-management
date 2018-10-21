9895654d5e5e4e4b29e721b2a7a45206a117115d
Otiono Gregory <itobitoast@gmail.com>
2018-10-21 19:17:00 +0100

    feature (test): test for delete route /api/v1/products/:id This is a test file [finishes #1613672800]

--- server/test/test.js
+++ server/test/test.js
@@ -43,63 +43,24 @@ describe('Products', () => {
         done();
       });
   });
+
+  it('It should DELETE PRODUCT on /api/v1/products/id DELETE', (done) => {
+    chai.request(app)
+      .get('/api/v1/products')
+      .end((error, response) => {
+        chai.request(app)
+          .delete('/api/v1/products/:id')
+          .end((error, response) => {
+            response.should.have.status(404);
+            response.should.be.json;
+            response.body.should.be.a('object');
+            response.body.should.have.property('message');
+            response.body.message.should.be.a('string');
+            done();
+          });
+      });
+  });
 });
-
-// // TEST FOR PRODUCTS
-// describe('Test for all products api', () => {
-//   it('Get all products', (done) => { // <= Pass in done callback
-//     chai.request(server)
-//       .get('/api/v1/products')
-//       .end((error, response) => {
-//         expect(response).to.have.status(200);
-//         done(); // <= Call done to signal callback end
-//       });
-//   });
-// });
-
-// describe('Test for a product api', () => {
-//   it('Get A products', (done) => { // <= Pass in done callback
-//     chai.request(server)
-//       .get('/api/v1/products/:id')
-//       .end((error, response) => {
-//         expect(response).to.have.status(404);
-//         done(); // <= Call done to signal callback end
-//       });
-//   });
-// });
-
-// describe('Test for a product api', () => {
-//   it('Get A products', (done) => { // <= Pass in done callback
-//     chai.request(server)
-//       .get('/api/v1/products/:id')
-//       .end((error, response) => {
-//         expect(response).to.have.status(404);
-//         done(); // <= Call done to signal callback end
-//       });
-//   });
-// });
-
-// describe('Test for a product api', () => {
-//   it('POST a product', (done) => { // <= Pass in done callback
-//     chai.request(server)
-//       .post('/api/v1/products')
-//       .end((error, response) => {
-//         expect(response).to.have.status(404);
-//         done(); // <= Call done to signal callback end
-//       });
-//   });
-// });
-
-// describe('Test for a product api', () => {
-//   it('DELETE a product', (done) => { // <= Pass in done callback
-//     chai.request(server)
-//       .delete('/api/v1/products/:id')
-//       .end((error, response) => {
-//         expect(response).to.have.status(404);
-//         done(); // <= Call done to signal callback end
-//       });
-//   });
-// });
 
 // // TEST FOR SALES
 // describe('Test for all sales api', () => {
