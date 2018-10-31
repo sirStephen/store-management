5e21e6d18e97eedf1a7c86ab37e36a7b2cf41a35
Otiono Gregory <itobitoast@gmail.com>
2018-10-31 22:54:05 +0100

    chore(ui): upload gh-pages

--- ui/assets/js/menu.js
+++ ui/assets/js/menu.js
@@ -1,27 +1,22 @@
-var menu = document.getElementById('menu');
-var overlay = document.getElementById('overlay');
-var cross = document.querySelector('.cross');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+const cross = document.querySelector('.cross');
 
-//Create function for on menu hover
+// Create function for on menu hover
 
-menu.addEventListener('click', function () {
-    overlay.animate([
-        {transform: 'translatex(0%)'},
-        {transform: 'translatex(100%)'}
-      ]);
-  
-  if (overlay.style.display === "block") {
-           overlay.style.display = "none";
-      } else {
-           overlay.style.display = "block";
-      } 
-  
+menu.addEventListener('click', () => {
+  overlay.animate([
+    { transform: 'translatex(0%)' },
+    { transform: 'translatex(100%)' },
+  ]);
 
+  if (overlay.style.display === 'block') {
+    overlay.style.display = 'none';
+  } else {
+    overlay.style.display = 'block';
+  }
 });
 
-cross.addEventListener('click', function () {
-  
-  overlay.style.display = "none";
-  
-  
+cross.addEventListener('click', () => {
+  overlay.style.display = 'none';
 });
