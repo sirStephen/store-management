2b25e5d9b78239582de399fc587f2efef086c3b7
Otiono Gregory <itobitoast@gmail.com>
2018-11-02 06:11:36 +0100

    more testing

--- server/test/sale.test.js
+++ server/test/sale.test.js
@@ -51,4 +51,46 @@ describe('Sales', () => {
         done();
       });
   });
+
+  it('It should get all sales on /api/v1/sales/:id POST', (done) => {
+    chai.request(app)
+      .get('/api/v1/sales/100')
+      .set('Authorization', user) 
+      .end((error, response) => {
+        console.log(response.body);
+        response.should.have.status(404);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('no sale found');
+        done();
+      });
+  });
+
+  it('It should get all sales on /api/v1/sales/:id POST', (done) => {
+    chai.request(app)
+      .get('/api/v1/sales/1')
+      .set('Authorization', user) 
+      .end((error, response) => {
+        console.log(response.body);
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('found sale');
+        done();
+      });
+  });
+
+  it('It should get all sales on /api/v1/sales/:id POST', (done) => {
+    chai.request(app)
+      .get('/api/v1/sales/a')
+      .set('Authorization', user) 
+      .end((error, response) => {
+        console.log(response.body);
+        response.should.have.status(406);
+        response.should.be.json;
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('The sale id should be an integer');
+        done();
+      });
+  });
 });
